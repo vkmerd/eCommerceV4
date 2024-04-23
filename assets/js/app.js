352eed0580618351eadf63c826397408f92da0fe
vkmerd <mert32139@gmail.com>
2024-04-23 23:17:26 +0300

    subcontainer açıldı ve localstorage ile veri çekildi

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,8 @@
 const productContainer = document.querySelector(".product")
 let limit = 100;
 const BASE_URL = `https://dummyjson.com/products?limit=${limit}`
+const itemcontainer = document.querySelector(".product-itemscontainer")
+const subcontainer = document.querySelector(".prod-subcontainer")
 
 const productFetch = async () => {
     const productBk = await fetch(`${BASE_URL}`);
@@ -12,18 +14,29 @@ const prdfr = async() => {
     const productitems = await productFetch()
     const products = productitems.products
     productContainer.innerHTML = ``
-    for(const productitems of products){
+    for(const producting of products){
        const productBox = document.createElement('div')
        productBox.classList.add("product-box")
        productBox.innerHTML += `
-            <img src="${productitems.thumbnail}" class="img-homeimages">
-            <h3 class="product-title">${productitems.title}</h3>
-            <p class="product-desc">${productitems.description}</p>
-            <p class="product-price">$${productitems.price}</p>
+            <img src="${producting.thumbnail}" class="img-homeimages">
+            <h3 class="product-title">${producting.title}</h3>
+            <p class="product-desc">${producting.description}</p>
+            <p class="product-price">$${producting.price}</p>
         `
         productContainer.appendChild(productBox);
         productBox.addEventListener("click",function(){
-            console.log(productitems.id);
+            localStorage.setItem(`${producting.id}`, JSON.stringify(producting))
+            itemcontainer.classList.add("gorunur")
+            const storedProducting = localStorage.getItem(`${producting.id}`);
+            if(storedProducting){
+                const parseProducting = JSON.parse(storedProducting)
+                subcontainer.innerHTML += `
+                    <img src="${parseProducting.thumbnail}">
+                `
+            }
+            else{
+                console.log("ürün bulunamadı");
+            }
         })
     }
 }
